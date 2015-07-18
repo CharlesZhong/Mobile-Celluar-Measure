4652312dfe62dfe327a5ce844f750218e97f4567
CharlesZhong <huatianjue@gmail.com>
2015-07-19 03:10:13 +0800

    add stat

--- http_parser/statistic.py
+++ http_parser/statistic.py
@@ -33,6 +33,8 @@ def stat_webp_compress(image_output_file):
     ori_size_statistic = defaultdict(int)
     compress_size_statistic_50 = defaultdict(int)
     compress_size_statistic_70 = defaultdict(int)
+    compress_size_statistic_75 = defaultdict(int)
+
     with open(image_output_file) as r_handler:
         for line in r_handler:
             try:
@@ -40,7 +42,7 @@ def stat_webp_compress(image_output_file):
                     overall_statistic['all'] += 1
                     line = line.strip()
                     terms = line.split('\t')
-                    if len(terms) != 23:
+                    if len(terms) != 26:
                         overall_statistic['format_wrong'] += 1
                         continue
 
@@ -50,8 +52,9 @@ def stat_webp_compress(image_output_file):
                     # print image_model.compress_size
 
                     len_response_body = int(terms[11])
-                    compress_size_50 = int(terms[-5])
-                    compress_size_70 = int(terms[-2])
+                    compress_size_50 = int(terms[-8])
+                    compress_size_70 = int(terms[-5])
+                    compress_size_75 = int(terms[-2])
 
 
                     real_type_count_statistic[terms[12]] += 1
@@ -59,14 +62,17 @@ def stat_webp_compress(image_output_file):
 
                     compress_size_statistic_50[terms[12]] += compress_size_50
                     compress_size_statistic_70[terms[12]] += compress_size_70
+                    compress_size_statistic_75[terms[12]] += compress_size_75
             except Exception as e:
                 overall_statistic['error'] += 1
                 logging.error("error {} in line {}".format(e, line))
 
     logging.info("[STAT] overstat is {}".format(overall_statistic))
     for item in ori_size_statistic:
-        print "{}\t{}\t{}\t{}\t{}\t{}\t{}".format(item, real_type_count_statistic[item],
+        print "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(item, real_type_count_statistic[item],
                                                   ori_size_statistic[item],
+                                            compress_size_statistic_75[item],
+                                          float(compress_size_statistic_75[item]) / ori_size_statistic[item],
                                           compress_size_statistic_70[item],
                                           float(compress_size_statistic_70[item]) / ori_size_statistic[item],
                                                   compress_size_statistic_50[item],
